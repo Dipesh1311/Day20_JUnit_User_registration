c8701a5e4ccabdd1f3f0df00c17827673f04f76c
Dipesh1311 <dipesh.rathod794@gmail.com>
2022-09-10 19:44:07 +0530

    Added implemented program for Test and validation for first name.

---
+++ src/test/java/com/bridgelabz/userregistration/UserRegistrationTest.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.userregistration;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class UserRegistrationTest {
+    @Test
+    public void testFirstName_WhenProper_ShouldReturnTrue() {
+        UserRegistration validator = new UserRegistration();
+        boolean result = validator.validateFirstName("Siva");
+        Assert.assertEquals(true, result);
+    }
+}
